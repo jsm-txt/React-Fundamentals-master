50fa2cee5167c64f7bd714a264995e689baccdee
jsm-txt <hose153@gmail.com>
2021-07-07 15:47:47 -0500

    dynamic content and footer

--- react_app/src/POPOSSpace.js
+++ react_app/src/POPOSSpace.js
@@ -1,22 +1,26 @@
 // src/Project.js
+import data from './sfpopos-data.json'
 
 import React from 'react'
-import logo from './logo.svg'
+
 import './POPOSSpace.css';
 
+const titles = data.map((obj) => {
+  return <h1>{obj.title}</h1>
+})
+
 function POPOSSpace(props) {
-  const { name, image, address } = props
+  const { name, image, address, hours } = props
   return (
-    <div>
-      <img
-        src={`${process.env.PUBLIC_URL}images/${image}`}
-        width="300"
-        height="300"
-        alt="Hello"
-      />
+    <div className="POPOSSpace">
       <h1>{name}</h1>
+      <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
       <div>{address}</div>
+      <div>{hours}</div>
     </div>
   )
 }
+
+
+console.log(titles)
 export default POPOSSpace
